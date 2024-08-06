7687fe4fdf8a7b1e941e577a581763609ccaa666
Tobias Winkler <tobias.winkler@omio.com>
2024-08-06 10:44:31 +0200

    export German Spitz
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- src/breeds.ts
+++ src/breeds.ts
@@ -1,7 +1,8 @@
 import DutchShepherd from "../db/fci/1/1/223";
 import FoxTerrierSmooth from "../db/fci/3/1/12";
 import FoxTerrierWire from "../db/fci/3/1/169";
+import GermanSpitz from "../db/fci/5/4/97";
 
 export default {
-  fci: [DutchShepherd, FoxTerrierSmooth, FoxTerrierWire],
+  fci: [DutchShepherd, FoxTerrierSmooth, FoxTerrierWire, GermanSpitz],
 };
