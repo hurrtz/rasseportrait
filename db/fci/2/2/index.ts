48f841cb2f6f5afc9fd3349142ae1112fc7d5f18
Tobias Winkler <tobias.winkler@omio.com>
2024-08-07 15:02:20 +0200

    add Broholmer
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- db/fci/2/2/index.ts
+++ db/fci/2/2/index.ts
@@ -3,6 +3,7 @@ import Boxer from "./144";
 import Rottweiler from "./147";
 import Landseer from "./226";
 import DogoArgentino from "./292";
+import Broholmer from "./315";
 
 export default {
   PyreneanMountainDog,
@@ -10,4 +11,5 @@ export default {
   Rottweiler,
   Landseer,
   DogoArgentino,
+  Broholmer,
 };
