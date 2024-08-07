ad489eeb259ab5897a1115cf72874e3be7f74a32
Tobias Winkler <tobias.winkler@omio.com>
2024-08-07 14:55:40 +0200

    add Dobermann
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

---
+++ db/fci/2/1/143/index.ts
@@ -0,0 +1,29 @@
+import type { Breed, FCI } from "../../../../../types/breed";
+import { getBreedImagePath } from "../../../../../src/utils";
+
+const fci: FCI = {
+  group: 2,
+  section: 1,
+  standardNumber: 143,
+};
+
+const breed: Breed = {
+  names: ["Dobermann"],
+  fci,
+  image: getBreedImagePath({ ...fci }),
+  podcast: [
+    {
+      episode: "New York, Husumer Protestschwein, Kupierte Ohren",
+      url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/81-new-york-husumer-protestschwein-kupierte-ohren-exok0jysekhyi",
+      timecode: 2776,
+    },
+  ],
+  furtherReading: [
+    {
+      name: "Wikipedia",
+      url: "https://de.wikipedia.org/wiki/Dobermann",
+    },
+  ],
+};
+
+export default breed;
--- db/fci/2/1/index.ts
+++ db/fci/2/1/index.ts
@@ -1,9 +1,11 @@
+import Dobermann from "./143";
 import GiantSchnauzer from "./181";
 import StandardSchnauzer from "./182";
 import MiniatureSchnauzer from "./183";
 import Affenpinscher from "./186";
 
 export default {
+  Dobermann,
   GiantSchnauzer,
   StandardSchnauzer,
   MiniatureSchnauzer,
