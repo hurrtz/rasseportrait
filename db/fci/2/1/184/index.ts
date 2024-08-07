250e23eda408ee8a091c5f79111ebba46b426dd6
Tobias Winkler <tobias.winkler@omio.com>
2024-08-07 16:02:04 +0200

    add German Pinscher
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

---
+++ db/fci/2/1/184/index.ts
@@ -0,0 +1,29 @@
+import type { Breed, FCI } from "../../../../../types/breed";
+import { getBreedImagePath } from "../../../../../src/utils";
+
+const fci: FCI = {
+  group: 2,
+  section: 1,
+  standardNumber: 184,
+};
+
+const breed: Breed = {
+  names: ["Deutscher Pinscher"],
+  fci,
+  image: getBreedImagePath({ ...fci }),
+  podcast: [
+    {
+      episode: "Bitte nicht nachmachen!",
+      url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/89-bitte-nicht-nachmachen-tt85sxkz7dhtd",
+      timecode: 1470,
+    },
+  ],
+  furtherReading: [
+    {
+      name: "Wikipedia",
+      url: "https://de.wikipedia.org/wiki/Deutscher_Pinscher",
+    },
+  ],
+};
+
+export default breed;
--- db/fci/2/1/index.ts
+++ db/fci/2/1/index.ts
@@ -2,6 +2,7 @@ import Dobermann from "./143";
 import GiantSchnauzer from "./181";
 import StandardSchnauzer from "./182";
 import MiniatureSchnauzer from "./183";
+import GermanPinscher from "./184";
 import Affenpinscher from "./186";
 import DanishSwedishFarmdog from "./356";
 
@@ -10,6 +11,7 @@ export default {
   GiantSchnauzer,
   StandardSchnauzer,
   MiniatureSchnauzer,
+  GermanPinscher,
   Affenpinscher,
   DanishSwedishFarmdog,
 };
