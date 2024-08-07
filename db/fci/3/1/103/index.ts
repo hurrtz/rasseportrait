3b992bd9c6e8ade44e63e1f0a42a535beed7980a
Tobias Winkler <tobias.winkler@omio.com>
2024-08-07 17:25:31 +0200

    add Jagdterrier
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

---
+++ db/fci/3/1/103/index.ts
@@ -0,0 +1,30 @@
+import type { Breed, FCI } from "../../../../../types/breed";
+import { getBreedImagePath } from "../../../../../src/utils";
+
+const fci: FCI = {
+  group: 3,
+  section: 1,
+  standardNumber: 103,
+};
+
+const breed: Breed = {
+  names: ["Deutscher Jagdterrier"],
+  fci,
+  image: getBreedImagePath({ ...fci }),
+  podcast: [
+    {
+      episode:
+        "Ein Keks mit Folgen, Körpersprache für Hunde & schöne Geschenke",
+      url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/65-ein-keks-mit-folgen-koerpersprache-fuer-hunde-schoene-geschenke-362zgzakgwpmb",
+      timecode: 2331,
+    },
+  ],
+  furtherReading: [
+    {
+      name: "Wikipedia",
+      url: "https://de.wikipedia.org/wiki/Deutscher_Jagdterrier",
+    },
+  ],
+};
+
+export default breed;
--- db/fci/3/1/index.ts
+++ db/fci/3/1/index.ts
@@ -1,4 +1,5 @@
 import FoxTerrierSmooth from "./12";
+import Jagdterrier from "./103";
 import FoxTerrierWire from "./169";
 
-export default { FoxTerrierSmooth, FoxTerrierWire };
+export default { FoxTerrierSmooth, Jagdterrier, FoxTerrierWire };
