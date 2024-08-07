99cbfab4758543423ee402f946d5baa493c0c2c4
Tobias Winkler <tobias.winkler@omio.com>
2024-08-07 16:19:19 +0200

    add Turnspit Dog
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

---
+++ db/Turnspit/index.ts
@@ -0,0 +1,38 @@
+import type { Breed, FCI } from "../../types/breed";
+import { getBreedImagePath } from "../../src/utils";
+
+const fci: FCI = {
+  group: -1,
+  section: -1,
+  standardNumber: -1,
+};
+
+const breed: Breed = {
+  names: [
+    "Turnspit",
+    "Turnspit Dog",
+    "Kitchen Dog",
+    "Cooking Dog",
+    "Underdog",
+    "Canis vertigus",
+    "Vernepator",
+    "Küchenhund",
+  ],
+  image: getBreedImagePath({ ...fci }),
+  fci,
+  podcast: [
+    {
+      episode: "Vorsicht vor Weihnachten",
+      url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/91-vorsicht-vor-weihnachten-8afvekcm6qtq4",
+      timecode: 1762,
+    },
+  ],
+  furtherReading: [
+    {
+      name: "Wikipedia",
+      url: "https://de.wikipedia.org/wiki/Turnspit_Dog",
+    },
+  ],
+};
+
+export default breed;
--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,6 @@
 import FCIBreeds from "./fci";
 import Elo from "./Elo";
 import PodencoAndaluz from "./PodencoAndaluz";
+import Turnspit from "./Turnspit";
 
-export default { ...FCIBreeds, Elo, PodencoAndaluz };
+export default { ...FCIBreeds, Elo, PodencoAndaluz, Turnspit };
