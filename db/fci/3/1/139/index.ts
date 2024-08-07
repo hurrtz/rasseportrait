87b30b8820d88301388fa067ccaba9e33658aa86
Tobias Winkler <tobias.winkler@omio.com>
2024-08-07 17:37:25 +0200

    add Irish Terrier
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

---
+++ db/fci/3/1/139/index.ts
@@ -0,0 +1,29 @@
+import type { Breed, FCI } from "../../../../../types/breed";
+import { getBreedImagePath } from "../../../../../src/utils";
+
+const fci: FCI = {
+  group: 3,
+  section: 1,
+  standardNumber: 139,
+};
+
+const breed: Breed = {
+  names: ["Irish Terrier"],
+  fci,
+  image: getBreedImagePath({ ...fci }),
+  podcast: [
+    {
+      episode: "Bibi & Tina, Horoskope und ein absurdes Rasse-Ranking",
+      url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/106-bibi-tina-horoskope-und-ein-absurdes-rasse-ranking-vzrl6k8ajwg7p",
+      timecode: 1890,
+    },
+  ],
+  furtherReading: [
+    {
+      name: "Wikipedia",
+      url: "https://de.wikipedia.org/wiki/Irish_Terrier",
+    },
+  ],
+};
+
+export default breed;
--- db/fci/3/1/index.ts
+++ db/fci/3/1/index.ts
@@ -1,5 +1,6 @@
 import FoxTerrierSmooth from "./12";
 import Jagdterrier from "./103";
+import IrishTerrier from "./139";
 import FoxTerrierWire from "./169";
 
-export default { FoxTerrierSmooth, Jagdterrier, FoxTerrierWire };
+export default { FoxTerrierSmooth, Jagdterrier, IrishTerrier, FoxTerrierWire };
