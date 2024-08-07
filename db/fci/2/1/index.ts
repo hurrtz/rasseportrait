bda6e8ff5367f0cf71c7e55841de1bf369a49653
Tobias Winkler <tobias.winkler@omio.com>
2024-08-07 13:41:09 +0200

    add Affenpinscher
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- db/fci/2/1/index.ts
+++ db/fci/2/1/index.ts
@@ -1,5 +1,11 @@
 import GiantSchnauzer from "./181";
 import StandardSchnauzer from "./182";
 import MiniatureSchnauzer from "./183";
+import Affenpinscher from "./186";
 
-export default { GiantSchnauzer, StandardSchnauzer, MiniatureSchnauzer };
+export default {
+  GiantSchnauzer,
+  StandardSchnauzer,
+  MiniatureSchnauzer,
+  Affenpinscher,
+};
