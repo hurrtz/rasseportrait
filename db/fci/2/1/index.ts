617e4b332637c63862cef3a721bcd0d50906b7de
Tobias Winkler <tobias.winkler@omio.com>
2024-08-07 10:19:05 +0200

    add Schnauzer
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

---
+++ db/fci/2/1/index.ts
@@ -0,0 +1,5 @@
+import GiantSchnauzer from "./181";
+import StandardSchnauzer from "./182";
+import MiniatureSchnauzer from "./183";
+
+export default { GiantSchnauzer, StandardSchnauzer, MiniatureSchnauzer };
