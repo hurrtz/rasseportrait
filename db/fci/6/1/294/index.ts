1e1218a788cc8748cedf494e610fc19c9465374f
Tobias Winkler <tobias.winkler@omio.com>
2024-08-08 19:22:35 +0200

    add Otterhound
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

---
+++ db/fci/6/1/294/index.ts
@@ -0,0 +1,29 @@
+import type { Breed, FCI } from "../../../../../types/breed";
+import { getBreedImagePath } from "../../../../../src/utils";
+
+const fci: FCI = {
+  group: 6,
+  section: 1,
+  standardNumber: 294,
+};
+
+const breed: Breed = {
+  names: ["Otterhund"],
+  fci,
+  image: getBreedImagePath({ ...fci }),
+  podcast: [
+    {
+      episode: "Otterjagd & Dackelkatzen",
+      url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/157-otterjagd-dackelkatzen-jfhrn0c2yrt91",
+      timecode: 2238,
+    },
+  ],
+  furtherReading: [
+    {
+      name: "Wikipedia",
+      url: "https://de.wikipedia.org/wiki/Otterhund",
+    },
+  ],
+};
+
+export default breed;
--- db/fci/6/1/index.ts
+++ db/fci/6/1/index.ts
@@ -1,11 +1,13 @@
 import GriffonNivernais from "./17";
 import GasconSaintongeois from "./21";
 import Beagle from "./161";
+import Otterhound from "./294";
 import BlackAndTanCoonhound from "./300";
 
 export default {
   GriffonNivernais,
   GasconSaintongeois,
   Beagle,
+  Otterhound,
   BlackAndTanCoonhound,
 };
