5041d7da3bf5ee929c8e9b75a0ddf2bd8fba1496
Tobias Winkler <tobias.winkler@omio.com>
2024-08-08 17:57:17 +0200

    add Irish Water Spaniel
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

---
+++ db/fci/8/3/124/index.ts
@@ -0,0 +1,34 @@
+import type { Breed, FCI } from "../../../../../types/breed";
+import { getBreedImagePath } from "../../../../../src/utils";
+
+const fci: FCI = {
+  group: 8,
+  section: 3,
+  standardNumber: 124,
+};
+
+const breed: Breed = {
+  names: [
+    "Irish Water Spaniel",
+    "Irischer Wasserspaniel",
+    "Perro de agua irlandés",
+    "Épagneul d’eau irlandais",
+  ],
+  fci,
+  image: getBreedImagePath({ ...fci }),
+  podcast: [
+    {
+      episode: "Stark wie ein Regenwurm",
+      url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/144-stark-wie-ein-regenwurm-yz5nu29vkfm1o",
+      timecode: 2909,
+    },
+  ],
+  furtherReading: [
+    {
+      name: "Wikipedia",
+      url: "https://de.wikipedia.org/wiki/Irish_Water_Spaniel",
+    },
+  ],
+};
+
+export default breed;
--- db/fci/8/3/index.ts
+++ db/fci/8/3/index.ts
@@ -1,5 +1,11 @@
+import IrishWaterSpaniel from "./124";
 import Wetterhoun from "./221";
 import LagottoRomagnolo from "./298";
 import SpanishWaterDog from "./336";
 
-export default { Wetterhoun, LagottoRomagnolo, SpanishWaterDog };
+export default {
+  IrishWaterSpaniel,
+  Wetterhoun,
+  LagottoRomagnolo,
+  SpanishWaterDog,
+};
