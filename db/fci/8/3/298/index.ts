c6becc228b3255d1d7bfa5e820e8cda56baece64
Tobias Winkler <tobias.winkler@omio.com>
2024-08-08 15:01:19 +0200

    add Lagotto Romagnolo
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

---
+++ db/fci/8/3/298/index.ts
@@ -0,0 +1,33 @@
+import type { Breed, FCI } from "../../../../../types/breed";
+import { getBreedImagePath } from "../../../../../src/utils";
+
+const fci: FCI = {
+  group: 8,
+  section: 3,
+  standardNumber: 298,
+};
+
+const breed: Breed = {
+  names: [
+    "Lagotto Romagnolo",
+    "Wasserhund der Romagna",
+    "Italienischer Trüffelhund",
+  ],
+  fci,
+  image: getBreedImagePath({ ...fci }),
+  podcast: [
+    {
+      episode: "Fichtenflitzer, Riesenregenwurm & Kot-Tapete",
+      url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/138-fichtenflitzer-riesenregenwurm-kot-tapete-lcq6dk9w7df3w",
+      timecode: 818,
+    },
+  ],
+  furtherReading: [
+    {
+      name: "Wikipedia",
+      url: "https://de.wikipedia.org/wiki/Lagotto_Romagnolo",
+    },
+  ],
+};
+
+export default breed;
--- db/fci/8/3/index.ts
+++ db/fci/8/3/index.ts
@@ -1,4 +1,5 @@
 import Wetterhoun from "./221";
+import LagottoRomagnolo from "./298";
 import SpanishWaterDog from "./336";
 
-export default { Wetterhoun, SpanishWaterDog };
+export default { Wetterhoun, LagottoRomagnolo, SpanishWaterDog };
