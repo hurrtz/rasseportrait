eebb979dd667e4038ed50ea8bc98125b53b3eefe
Tobias Winkler <tobias.winkler@omio.com>
2024-08-08 14:54:22 +0200

    add Nova Scotia Duck Tolling Retriever
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

---
+++ db/fci/8/1/312/index.ts
@@ -0,0 +1,29 @@
+import type { Breed, FCI } from "../../../../../types/breed";
+import { getBreedImagePath } from "../../../../../src/utils";
+
+const fci: FCI = {
+  group: 8,
+  section: 1,
+  standardNumber: 312,
+};
+
+const breed: Breed = {
+  names: ["Nova Scotia Duck Tolling Retriever"],
+  fci,
+  image: getBreedImagePath({ ...fci }),
+  podcast: [
+    {
+      episode: "Positive Verstärkung, Männergrippe & Zurückrudern",
+      url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/136-positive-verstaerkung-maennergrippe-zurueckrudern-pcewcbc9hru3t",
+      timecode: 1380,
+    },
+  ],
+  furtherReading: [
+    {
+      name: "Wikipedia",
+      url: "https://de.wikipedia.org/wiki/Nova_Scotia_Duck_Tolling_Retriever",
+    },
+  ],
+};
+
+export default breed;
--- db/fci/8/1/index.ts
+++ db/fci/8/1/index.ts
@@ -1,5 +1,11 @@
 import GoldenRetriever from "./111";
 import FlatCoatedRetriever from "./121";
 import LabradorRetriever from "./122";
+import NovaScotiaDuckTollingRetriever from "./312";
 
-export default { GoldenRetriever, FlatCoatedRetriever, LabradorRetriever };
+export default {
+  GoldenRetriever,
+  FlatCoatedRetriever,
+  LabradorRetriever,
+  NovaScotiaDuckTollingRetriever,
+};
