08e3e4f4b87dace68629a2cd9de550d6365e908b
Tobias Winkler <tobias.winkler@omio.com>
2024-08-08 18:32:36 +0200

    add Shetland Sheepdog
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

---
+++ db/fci/1/1/88/index.ts
@@ -0,0 +1,29 @@
+import type { Breed, FCI } from "../../../../../types/breed";
+import { getBreedImagePath } from "../../../../../src/utils";
+
+const fci: FCI = {
+  group: 1,
+  section: 1,
+  standardNumber: 88,
+};
+
+const breed: Breed = {
+  names: ["Shetland Sheepdog"],
+  fci,
+  image: getBreedImagePath({ ...fci }),
+  podcast: [
+    {
+      episode: "Parlamentskreis Hund",
+      url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/153-parlamentskreis-hund-yijtj5yrhqgo1",
+      timecode: 2232,
+    },
+  ],
+  furtherReading: [
+    {
+      name: "Wikipedia",
+      url: "https://de.wikipedia.org/wiki/Shetland_Sheepdog",
+    },
+  ],
+};
+
+export default breed;
--- db/fci/1/1/index.ts
+++ db/fci/1/1/index.ts
@@ -4,6 +4,7 @@ import Beauceron from "./44";
 import Komondor from "./53";
 import Kuvasz from "./54";
 import Pumi from "./56";
+import ShetlandSheepdog from "./88";
 import Briard from "./113";
 import GermanShepherd from "./166";
 import DutchShepherd from "./223";
@@ -16,6 +17,7 @@ export default {
   Komondor,
   Kuvasz,
   Pumi,
+  ShetlandSheepdog,
   Briard,
   GermanShepherd,
   DutchShepherd,
