716ab1c31cc4db9a7b7597c2c01d60a5070411d0
Tobias Winkler <tobias.winkler@omio.com>
2024-08-10 12:54:25 +0200

    add Spinone Italiano
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

---
+++ db/fci/7/1/165/index.ts
@@ -0,0 +1,45 @@
+import type { Breed, FCI } from "../../../../../types/breed";
+import { getBreedImagePath } from "../../../../../src/utils";
+
+const fci: FCI = {
+  group: 7,
+  section: 1,
+  standardNumber: 165,
+};
+
+const { standardNumber: id } = fci;
+
+const breed: Breed = {
+  id,
+  names: [
+    "Spinone Italiano",
+    "Spinone",
+    "Rauhaariger italienischer Vorstehhund",
+    "Italian Griffon",
+  ],
+  fci,
+  image: getBreedImagePath({ ...fci }),
+  podcast: [
+    {
+      episode: "Summer Edition #1: Geburtstag & Good News",
+      url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/summer-edition-1-geburtstag-good-news-8jh3dh6u48lpr",
+      timecode: 1,
+    },
+  ],
+  furtherReading: [
+    {
+      name: "Wikipedia",
+      url: "https://de.wikipedia.org/wiki/Berger_de_Picardie",
+    },
+    {
+      name: "FCI",
+      url: "https://www.fci.be/de/nomenclature/ITALIENISCHER-SPINONE-165.html",
+    },
+    {
+      name: "VDH",
+      url: "https://welpen.vdh.de/hunderassen/rasselexikon/ergebnis/spinone-italiano",
+    },
+  ],
+};
+
+export default breed;
--- db/fci/7/1/index.ts
+++ db/fci/7/1/index.ts
@@ -1,6 +1,7 @@
 import Weimaraner from "./99";
 import Viszlar from "./57";
 import LargeMuensterlaender from "./118";
+import SpinoneItaliano from "./165";
 import GermanRoughHairedPointer from "./232";
 import WireHairedViszlar from "./239";
 
@@ -8,6 +9,7 @@ export default {
   Weimaraner,
   Viszlar,
   LargeMuensterlaender,
+  SpinoneItaliano,
   GermanRoughHairedPointer,
   WireHairedViszlar,
 };
