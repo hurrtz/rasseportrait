f2f10a6924647bffcd8ab35e0880491b3965484e
Tobias Winkler <tobias.winkler@omio.com>
2024-08-10 14:02:54 +0200

    update information on Foxterrier
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- db/fci/3/1/12/index.ts
+++ db/fci/3/1/12/index.ts
@@ -18,18 +18,26 @@ const breed: Breed = {
     {
       episode: "Summer Edition #3: Martins Solo-Folge",
       url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/summer-edition-3-martins-solo-folge-tfmv41s6tzg65",
-      timecode: 0,
+      timecode: 948,
     },
     {
       episode: "Summer Edition #5: Grausame Natur",
       url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/summer-edition-5-grausame-natur-2cmtu8gbfflgx",
-      timecode: 0,
+      timecode: 277,
     },
   ],
   furtherReading: [
     {
       name: "Wikipedia",
       url: "https://de.wikipedia.org/wiki/Foxterrier",
+    },
+    {
+      name: "FCI",
+      url: "https://www.fci.be/de/nomenclature/FOX-TERRIER-GLATTHAAR-12.html",
+    },
+    {
+      name: "VDH",
+      url: "https://welpen.vdh.de/hunderassen/rasselexikon/ergebnis/fox-terrier-glatthaar",
     },
   ],
 };
