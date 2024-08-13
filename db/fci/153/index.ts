e0d3af651cf16770c609d026d7c1881d74428470
Tobias Winkler <tobias.winkler@omio.com>
2024-08-13 19:48:03 +0200

    update details on Dalmatian
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- db/fci/153/index.ts
+++ db/fci/153/index.ts
@@ -16,6 +16,12 @@ const breed: Breed = {
   image: getBreedImagePath({ ...fci }),
   podcast: [
     {
+      number: 12,
+      episode: "Die ganze Wahrheit",
+      url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/12-die-ganze-wahrheit-kc3oyphk7j1ra",
+      timecode: 944,
+    },
+    {
       number: 105,
       episode: "Geiselnahme, skurrile Wildtiere & 1,5 Grad wärmer",
       url: "https://plus.rtl.de/podcast/tierisch-menschlich-der-podcast-mit-hundeprofi-martin-ruetter-und-katharina-adick-m5iuweomug8fv/105-geiselnahme-skurrile-wildtiere-1-5-grad-waermer-9qzro9uynb4pc",
