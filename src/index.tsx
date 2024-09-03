017abede520f0199a69465c0e4bac1e962d24129
Tobias Winkler <tobias.winkler@omio.com>
2024-09-03 22:48:31 +0200

    chore: remove redundant call to amplitude tracking
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React, { StrictMode } from "react";
 import * as amplitude from "@amplitude/analytics-browser";
+import { sessionReplayPlugin } from "@amplitude/plugin-session-replay-browser";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-amplitude.init("73172d06233b85ff451f0f15f016ec0b", { autocapture: true });
+const sessionReplayTracking = sessionReplayPlugin();
+amplitude.add(sessionReplayTracking);
+amplitude.init(
+  window.location.hostname === "localhost"
+    ? "" // no tracking during development
+    : "73172d06233b85ff451f0f15f016ec0b",
+);
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
