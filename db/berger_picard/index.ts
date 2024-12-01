c149f45becd5ee8c9aa71b6101a931668722f672
Tobias Winkler <tobias.winkler@omio.com>
2024-12-01 16:50:49 +0100

    chore: update information for Berger de Picardie
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- db/berger_picard/index.ts
+++ db/berger_picard/index.ts
@@ -3,6 +3,7 @@ import type { Breed } from "../../types/breed";
 const breed: Breed = {
   id: "berger_picard",
   names: ["Berger de Picardie", "Berger Picard"],
+  wasGuessedCorrectlyInPodcast: true,
   fci: {
     group: 1,
     section: 1,
