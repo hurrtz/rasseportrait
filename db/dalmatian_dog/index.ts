8096ac2ac19b737d3a5d350c2dae5d93cdab358c
Tobias Winkler <tobias.winkler@omio.com>
2024-12-01 15:41:39 +0100

    chore: update information for Dalmation
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- db/dalmatian_dog/index.ts
+++ db/dalmatian_dog/index.ts
@@ -38,6 +38,10 @@ const breed: Breed = {
       name: "VDH",
       url: "https://welpen.vdh.de/hunderassen/rasselexikon/ergebnis/dalmatiner",
     },
+    {
+      name: "Martin Rütter",
+      url: "https://www.martinruetter.com/rassekunde/dalmatiner-1",
+    },
   ],
 };
 
