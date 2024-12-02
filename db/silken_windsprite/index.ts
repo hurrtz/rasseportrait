e93a9732763433681614ae562ba8d7afebe7697a
Tobias Winkler <tobias.winkler@omio.com>
2024-12-02 22:09:01 +0100

    chore: update information for Silken Windsprite
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- db/silken_windsprite/index.ts
+++ db/silken_windsprite/index.ts
@@ -3,6 +3,7 @@ import type { Breed } from "../../types/breed";
 const breed: Breed = {
   id: "silken_windsprite",
   names: ["Silken Windsprite", "Longhaired Whippet", "Langhaar Whippet"],
+  wasGuessedCorrectlyInPodcast: false,
   fci: {
     group: -1,
     section: -1,
