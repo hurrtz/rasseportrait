618113a6082c8e8d183ce73ca6b5aae900dc72df
Tobias Winkler <tobias.winkler@omio.com>
2024-12-02 22:09:53 +0100

    chore: update information for Shetland Sheepdog
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- db/shetland_sheepdog/index.ts
+++ db/shetland_sheepdog/index.ts
@@ -3,6 +3,7 @@ import type { Breed } from "../../types/breed";
 const breed: Breed = {
   id: "shetland_sheepdog",
   names: ["Shetland Sheepdog"],
+  wasGuessedCorrectlyInPodcast: true,
   fci: {
     group: 1,
     section: 1,
