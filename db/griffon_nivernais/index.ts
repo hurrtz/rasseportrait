069cb3077e45aae7a14a676a30269d1a01a1cc24
Tobias Winkler <tobias.winkler@omio.com>
2024-12-02 22:22:55 +0100

    chore: update information for Griffon Nivernais
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- db/griffon_nivernais/index.ts
+++ db/griffon_nivernais/index.ts
@@ -3,6 +3,7 @@ import type { Breed } from "../../types/breed";
 const breed: Breed = {
   id: "griffon_nivernais",
   names: ["Griffon Nivernais"],
+  wasGuessedCorrectlyInPodcast: true,
   fci: {
     group: 6,
     section: 1,
