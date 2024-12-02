6377702589fd0774e832dfff875beda3cde19386
Tobias Winkler <tobias.winkler@omio.com>
2024-12-02 22:17:39 +0100

    chore: update information for Louisiana Catahoula Leopard Dog
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- db/louisiana_catahoula_leopard_dog/index.ts
+++ db/louisiana_catahoula_leopard_dog/index.ts
@@ -3,6 +3,7 @@ import type { Breed } from "../../types/breed";
 const breed: Breed = {
   id: "louisiana_catahoula_leopard_dog",
   names: ["Louisiana Catahoula Leopard Dog"],
+  wasGuessedCorrectlyInPodcast: true,
   fci: {
     group: -1,
     section: -1,
