f8ca364c6bcc74731af4239248d606bd114d6b9f
Tobias Winkler <tobias.winkler@omio.com>
2024-12-02 22:28:41 +0100

    chore: update information for Chinese Crested Dog
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- db/chinese_crested_dog/index.ts
+++ db/chinese_crested_dog/index.ts
@@ -3,6 +3,7 @@ import type { Breed } from "../../types/breed";
 const breed: Breed = {
   id: "chinese_crested_dog",
   names: ["Chinesischer Schopfhund"],
+  wasGuessedCorrectlyInPodcast: true,
   variants: [
     {
       names: ["Haarlos"],
