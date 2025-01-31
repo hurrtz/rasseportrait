6b9c37ffa29e8fe624ca2894039e9b8551e412cb
Tobias Winkler <tobias.winkler@omio.com>
2025-01-31 12:56:01 +0100

    feat: add dummy page Tipps
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

---
+++ src/pages/Tipps.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+const Tipps = () => {
+  const isMobile = useMediaQuery("(max-width: 480px)");
+
+  return (
+    <Box>
+      <Typography variant={isMobile ? "h4" : "h2"} gutterBottom>
+        Tipps
+      </Typography>
+
+      <Typography variant="body2" gutterBottom>
+        dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.
+      </Typography>
+    </Box>
+  );
+};
+
+export default Tipps;
