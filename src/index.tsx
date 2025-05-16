c62321fa4f59f12ffea38e64cfe7b25022f729d2
Tobias Winkler <tobias.winkler@omio.com>
2025-05-16 22:07:44 +0200

    feat: initialize v2 of Rasseportrait
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,12 @@
 import React, { StrictMode } from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import * as amplitude from "@amplitude/analytics-browser";
-import { sessionReplayPlugin } from "@amplitude/plugin-session-replay-browser";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-
-const sessionReplayTracking = sessionReplayPlugin();
-amplitude.add(sessionReplayTracking);
-amplitude.init(
-  window.location.hostname === "localhost"
-    ? "" // no tracking during development
-    : "73172d06233b85ff451f0f15f016ec0b",
-);
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 
 root.render(
   <StrictMode>
-    <CssBaseline />
     <App />
   </StrictMode>,
 );
--- src/pages/index.ts
+++
@@ -1,7 +0,0 @@
-import BreedList from "./BreedList";
-import Topics from "./Topics";
-import Tipps from "./Tipps";
-import DDDHNB from "./DDDHNB";
-import Imprint from "./Imprint";
-
-export { BreedList, Topics, Tipps, DDDHNB, Imprint };
