1959dc04a637db4af0e1114c36b887afb4fa8203
Tobias Winkler <tobias.winkler@omio.com>
2025-05-20 19:35:46 +0200

    feat: add page skeleton
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,7 @@ import {
 } from "@mantine/core";
 import type { Route } from "./+types/root";
 import "./app.css";
+import AppWrapper from "./App";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -39,7 +40,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        <MantineProvider>
+          <AppWrapper>{children}</AppWrapper>
+        </MantineProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
