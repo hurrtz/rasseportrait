0d7de935464aa31a69149d00ae9a31c5a6c7e654
Tobias Winkler <tobias.winkler@omio.com>
2025-05-21 09:06:52 +0200

    feat: rudimentary breed card
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

---
+++ app/components/BreedCard/BreedCard.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
+
+const BreedCard = () => {
+  const { Section } = Card;
+
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Section>
+        <Image
+          src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+          height={160}
+          alt="Norway"
+        />
+      </Section>
+
+      <Group justify="space-between" mt="md" mb="xs">
+        <Text fw={500}>Norway Fjord Adventures</Text>
+        <Badge color="pink">On Sale</Badge>
+      </Group>
+
+      <Text size="sm" c="dimmed">
+        With Fjord Tours you can explore more of the magical fjord landscapes
+        with tours and activities on and around the fjords of Norway
+      </Text>
+
+      <Button color="blue" fullWidth mt="md" radius="md">
+        Book classic tour now
+      </Button>
+    </Card>
+  );
+};
+
+export default BreedCard;
--- app/pages/rasseportrait/Rasseportrait.tsx
+++ app/pages/rasseportrait/Rasseportrait.tsx
@@ -1,3 +1,31 @@
-const Rasseportrait = () => <div>Rasseportrait Content</div>;
+import React from "react";
+import { Grid } from "@mantine/core";
+import { BreedCard } from "../../components/BreedCard";
+
+const Rasseportrait = () => {
+  const { Col } = Grid;
+
+  return (
+    <div>
+      <Grid grow>
+        <Col span={4}>
+          <BreedCard />
+        </Col>
+        <Col span={4}>
+          <BreedCard />
+        </Col>
+        <Col span={4}>
+          <BreedCard />
+        </Col>
+        <Col span={4}>
+          <BreedCard />
+        </Col>
+        <Col span={4}>
+          <BreedCard />
+        </Col>
+      </Grid>
+    </div>
+  );
+};
 
 export default Rasseportrait;
