cafad92a98f73ed459240cae6be0ebcc6ec73cdd
Tobias Winkler <tobias.winkler@omio.com>
2025-05-21 09:34:46 +0200

    feat: enable prod compression
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    viteCompression({
+      algorithm: "gzip",
+      compressionOptions: { level: 9 },
+    }),
+    viteCompression({
+      algorithm: "brotliCompress",
+      compressionOptions: { level: 11 },
+    }),
+  ],
 });
