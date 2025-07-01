bf0447cc1e97986ca6341a9962b3ea6e4aa57f3d
Tobias Winkler <tobias.winkler@omio.com>
2025-07-01 15:33:29 +0200

    feat: provide content for statistics page
    Signed-off-by: Tobias Winkler <tobias.winkler@omio.com>

--- app/pages/Statistics/Statistics.tsx
+++ app/pages/Statistics/Statistics.tsx
@@ -1,13 +1,37 @@
 import React from "react";
 import "./styles.css";
-import { Divider, Group, Progress, Stack, Text } from "@mantine/core";
-import { IconHandFingerRight } from "@tabler/icons-react";
+import {
+  Group,
+  Progress,
+  Stack,
+  Text,
+  Card,
+  NumberFormatter,
+  Button,
+} from "@mantine/core";
 import { getStatistics } from "../rasseportrait/utils";
-import { useRawBreeds } from "../../stores/breeds";
-import { AMOUNT_OF_BREEDS_TOTAL } from "../rasseportrait/constants";
+import {
+  useAllBreeds,
+  useBreedActions,
+  useRawBreeds,
+} from "../../stores/breeds";
+import {
+  AMOUNT_OF_BREEDS_TOTAL,
+  AMOUNT_OF_BREEDS_APPROVED,
+  AMOUNT_OF_BREEDS_PROVISIONAL,
+} from "../rasseportrait/constants";
+import type { Breed } from "types/breed";
+import { useDisclosure } from "@mantine/hooks";
+import { Modal } from "../../components/Modal";
 
 const Statistics = () => {
   const rawBreeds = useRawBreeds();
+  const allBreeds = useAllBreeds();
+
+  const [isModalOpen, { open: openModal, close: closeModal }] =
+    useDisclosure(false);
+
+  const { setSelectedBreed } = useBreedActions();
 
   const {
     amountBreedsPresented,
@@ -22,94 +46,217 @@ const Statistics = () => {
     katharinaCorrectGuessesPercentage,
   } = getStatistics(rawBreeds);
 
+  const handleBreedClick = (id: Breed["id"]) => {
+    const selectedBreedData = allBreeds.find((breed) => breed.id === id);
+
+    if (selectedBreedData) {
+      setSelectedBreed(id);
+      openModal();
+    }
+  };
+
+  const onCloseModal = () => {
+    closeModal();
+    setSelectedBreed(undefined);
+  };
+
   return (
-    <Stack>
-      <Group wrap="nowrap">
-        <IconHandFingerRight
-          width={24}
-          height={24}
-          style={{ flex: "0 0 auto" }}
-        />
-        <Stack w="100%">
-          <Text>Fortschritt Vorstellung FCI-Rasseliste</Text>
-          <Progress.Root size="xl">
-            <Progress.Section
-              value={Number(percentageBreedsPresented)}
-              color="#EEA13B"
-            >
-              <Progress.Label>
-                {percentageBreedsPresented}% ({amountBreedsPresented}/
-                {AMOUNT_OF_BREEDS_TOTAL})
-              </Progress.Label>
-            </Progress.Section>
-          </Progress.Root>
-        </Stack>
-      </Group>
-      <Divider />
-      <Group wrap="nowrap" grow={false}>
-        <IconHandFingerRight
-          width={24}
-          height={24}
-          style={{ flex: "0 0 auto" }}
-        />
-        <Stack>
-          <Text>Vorgestellte Rassen außerhalb der FCI-Rasseliste</Text>
-          <div>
-            {breedsOutsideFCI
-              .map((breed) => breed.details.public[0])
-              .join(", ")}
-          </div>
-        </Stack>
-      </Group>
-      <Divider />
-      <Group wrap="nowrap" grow={false}>
-        <IconHandFingerRight
-          width={24}
-          height={24}
-          style={{ flex: "0 0 auto" }}
-        />
-        <Stack>
-          <Text>
-            Erwähnte Rassen, die noch nicht offiziell vorgestellt wurden
-          </Text>
-          <div>
-            {breedsNotPresented
-              .map((breed) => breed.details.public[0])
-              .join(", ")}
-          </div>
-        </Stack>
-      </Group>
-      <Divider />
-      <Group wrap="nowrap" grow={false}>
-        <IconHandFingerRight
-          width={24}
-          height={24}
-          style={{ flex: "0 0 auto" }}
-        />
-        <Stack>
-          <Text>Erfolgsquote von Martin beim Erraten der Rassen</Text>
-          <div>
-            {martinCorrectGuessesPercentage}% ({martinCorrectGuesses}/
-            {martinCorrectGuessesOutOfTotal})
-          </div>
-        </Stack>
-      </Group>
-      <Divider />
-      <Group wrap="nowrap" grow={false}>
-        <IconHandFingerRight
-          width={24}
-          height={24}
-          style={{ flex: "0 0 auto" }}
-        />
-        <Stack>
-          <Text>Erfolgsquote von Katharina beim Erraten der Rassen</Text>
-          <div>
-            {katharinaCorrectGuessesPercentage}% ({katharinaCorrectGuesses}/
-            {katharinaCorrectGuessesOutOfTotal})
-          </div>
-        </Stack>
-      </Group>
-    </Stack>
+    <>
+      <Stack>
+        <Card withBorder shadow="sm" padding="lg">
+          <Stack>
+            <Text fw={300} c="dark" style={{ fontVariant: "small-caps" }}>
+              Fortschritt Vorstellung FCI-Rasseliste
+            </Text>
+          </Stack>
+
+          <Stack pt="md">
+            <Progress.Root size="xxl" h={24} w="100%">
+              <Progress.Section
+                value={Number(percentageBreedsPresented)}
+                color="#EEA13B"
+              >
+                <Progress.Label fw={300}>
+                  <NumberFormatter
+                    value={percentageBreedsPresented}
+                    suffix="%"
+                    thousandSeparator="."
+                    decimalSeparator=","
+                  />
+                </Progress.Label>
+              </Progress.Section>
+            </Progress.Root>
+          </Stack>
+
+          <Stack pt="xs" gap="5">
+            <Text size="sm" fw={300} c="dimmed">
+              Anzahl aller Rassen per FCI-Rasseliste:{" "}
+              <Text component="span" fw={900}>
+                {AMOUNT_OF_BREEDS_TOTAL}
+              </Text>{" "}
+              ({AMOUNT_OF_BREEDS_APPROVED} genehmigte Rassen +{" "}
+              {AMOUNT_OF_BREEDS_PROVISIONAL} vorläufige Rassen)
+            </Text>
+            <Text size="sm" fw={300} c="dimmed">
+              Anzahl der vorgestellten Rassen:{" "}
+              <Text component="span" fw={900}>
+                {amountBreedsPresented}
+              </Text>
+            </Text>
+          </Stack>
+        </Card>
+
+        <Card withBorder shadow="sm">
+          <Stack>
+            <Text fw={300} c="dark" style={{ fontVariant: "small-caps" }}>
+              Vorgestellte Rassen außerhalb der FCI-Rasseliste
+            </Text>
+          </Stack>
+
+          <Group pt="xs" gap="5">
+            {breedsOutsideFCI.map((breed) => (
+              <Button
+                key={breed.id}
+                variant="light"
+                size="xs"
+                c="dimmed"
+                fw={300}
+                style={{ fontVariant: "small-caps" }}
+                onClick={() => handleBreedClick(breed.id)}
+              >
+                {breed.details.public[0]}
+              </Button>
+            ))}
+          </Group>
+        </Card>
+
+        <Card withBorder shadow="sm">
+          <Stack>
+            <Text fw={300} c="dark" style={{ fontVariant: "small-caps" }}>
+              Erwähnte Rassen, die noch nicht offiziell vorgestellt wurden
+            </Text>
+          </Stack>
+
+          <Group pt="xs" gap="5">
+            {breedsNotPresented.map((breed) => (
+              <Button
+                key={breed.id}
+                variant="light"
+                size="xs"
+                c="dimmed"
+                fw={300}
+                style={{ fontVariant: "small-caps" }}
+                onClick={() => handleBreedClick(breed.id)}
+              >
+                {breed.details.public[0]}
+              </Button>
+            ))}
+          </Group>
+        </Card>
+
+        <Card withBorder shadow="sm">
+          <Stack>
+            <Text fw={300} c="dark" style={{ fontVariant: "small-caps" }}>
+              Erfolgsquote beim Erraten der Rassen
+            </Text>
+          </Stack>
+
+          <Group wrap="nowrap">
+            <Stack w="100%">
+              <Stack pt="md">
+                <Text
+                  fw={600}
+                  size="sm"
+                  c="dark"
+                  style={{ fontVariant: "small-caps" }}
+                >
+                  Martin
+                </Text>
+                <Progress.Root size="xxl" h={24} w="100%">
+                  <Progress.Section
+                    value={Number(martinCorrectGuessesPercentage)}
+                    color="#EEA13B"
+                  >
+                    <Progress.Label fw={300}>
+                      <NumberFormatter
+                        value={martinCorrectGuessesPercentage}
+                        suffix="%"
+                        thousandSeparator="."
+                        decimalSeparator=","
+                      />
+                    </Progress.Label>
+                  </Progress.Section>
+                </Progress.Root>
+              </Stack>
+
+              <Stack pt="0">
+                <Text fw={300} c="dimmed" size="sm">
+                  von{" "}
+                  <Text component="span" fw={900}>
+                    {martinCorrectGuessesOutOfTotal}
+                  </Text>{" "}
+                  erratbaren Rassen wurden{" "}
+                  <Text component="span" fw={900}>
+                    {martinCorrectGuesses}
+                  </Text>{" "}
+                  korrekt erraten (abzüglich{" "}
+                  <Button
+                    variant="light"
+                    size="xs"
+                    c="dimmed"
+                    fw={300}
+                    style={{ fontVariant: "small-caps" }}
+                    onClick={() => handleBreedClick(336)}
+                  >
+                    Spanischer Wasserhund
+                  </Button>
+                  )
+                </Text>
+              </Stack>
+            </Stack>
+
+            <Stack w="100%">
+              <Stack pt="md">
+                <Text fw={600} c="dark" size="sm">
+                  Katharina
+                </Text>
+                <Progress.Root size="xxl" h={24} w="100%">
+                  <Progress.Section
+                    value={Number(katharinaCorrectGuessesPercentage)}
+                    color="#EEA13B"
+                  >
+                    <Progress.Label fw={300}>
+                      <NumberFormatter
+                        value={katharinaCorrectGuessesPercentage}
+                        suffix="%"
+                        thousandSeparator="."
+                        decimalSeparator=","
+                      />
+                    </Progress.Label>
+                  </Progress.Section>
+                </Progress.Root>
+              </Stack>
+
+              <Stack pt="0">
+                <Text fw={300} c="dimmed" size="sm">
+                  von{" "}
+                  <Text component="span" fw={900}>
+                    {katharinaCorrectGuessesOutOfTotal}
+                  </Text>{" "}
+                  erratbaren Rasse wurde{" "}
+                  <Text component="span" fw={900}>
+                    {katharinaCorrectGuesses}
+                  </Text>{" "}
+                  korrekt erraten
+                </Text>
+              </Stack>
+            </Stack>
+          </Group>
+        </Card>
+      </Stack>
+
+      <Modal isOpen={isModalOpen} close={onCloseModal} />
+    </>
   );
 };
 
