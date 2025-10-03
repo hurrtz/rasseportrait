3a6b0b59bdfb9f5a656e3e3d412a24ed2af43765
Tobias Winkler <tobias.winkler@omio.com>
2025-10-03 20:04:12 +0200

    test: setup React Testing Library infrastructure and jsdom environment

--- jest.config.js
+++ jest.config.js
@@ -4,15 +4,21 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 export default {
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   transform: {
     ...tsJestTransformCfg,
   },
   moduleNameMapper: {
     "^~/(.*)$": "<rootDir>/app/$1",
+    "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/styleMock.js",
   },
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[jt]s?(x)",
+  ],
 };
